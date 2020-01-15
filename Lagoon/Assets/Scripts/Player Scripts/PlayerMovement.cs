955fc6289d1e873c69f4adaf2ed6828379edf848
Hugh <1700792@uad.ac.uk>
2020-01-15 13:52:54 +0000

    Test Push

---
+++ Lagoon/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    //Movement direction indicated by the left analogue stick
+    Vector3 MovementDirection = new Vector2();
+
+    //Speed that the player moves at
+
+    [SerializeField] float MovementSpeed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void HandleInput()
+    {
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("PlayerLH") * MovementSpeed * Time.deltaTime;
+        float z = Input.GetAxisRaw("PlayerLV") * MovementSpeed * Time.deltaTime;
+
+        MovementDirection = new Vector3(x, 0, z);   
+
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(MovementDirection * MovementSpeed, ForceMode.VelocityChange);
+
+        
+    }
+}
