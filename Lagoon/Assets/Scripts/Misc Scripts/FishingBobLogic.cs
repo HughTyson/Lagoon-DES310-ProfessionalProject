ae63d4bda8cfd326dc9d6f0fb9be736e5d461dce
Tomas Hudak <hudakt05@hotmail.com>
2020-01-19 15:05:14 +0000

    Fishing Mechanic
    Started working on the fishing mechanics.
    
    - buoyancy physics for fishing bob
    - fixed force fishing cast done with controller (RB)

---
+++ Lagoon/Assets/Scripts/Misc Scripts/FishingBobLogic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingBobLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField] float bobBoiyancy;
+    [SerializeField] float bobAirDrag;
+    [SerializeField] float bobWaterDrag;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    // Reminder:: Occurs at a FixedTimeStep
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Water")
+        {
+            GetComponentInParent<Rigidbody>().drag = bobWaterDrag;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Water")
+        {
+            if (other.bounds.Contains(transform.position))
+            {
+                Vector3 force = transform.up * (-Physics.gravity.y + bobBoiyancy);
+                GetComponentInParent<Rigidbody>().AddRelativeForce(force, ForceMode.Acceleration);
+            }
+
+        }
+
+      
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Water")
+        {
+           GetComponentInParent<Rigidbody>().drag = bobAirDrag;
+        }
+
+    }
+}
---
+++ Lagoon/Assets/Scripts/Player Scripts/PlayerFishing.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFishing : MonoBehaviour
+{
+
+    enum FISHING_STATE
+    { 
+    NOT_FISHING,
+    CASTING,
+    CASTED,
+    REELING
+    };
+
+
+
+    // Start is called before the first frame update
+    [SerializeField] GameObject prefabFishingBob;
+
+    [SerializeField] Transform cameraTransform;
+
+    [SerializeField] float fishingCastPowerMin;
+    [SerializeField] float fishingCastPowerMax;
+
+    Transform playerTransform;
+
+
+    private FISHING_STATE fishing_state;
+    private GameObject istantanceFishingBob;
+    void Start()
+    {
+        playerTransform = GetComponentInParent<Transform>();
+        fishing_state = FISHING_STATE.NOT_FISHING;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("PlayerRB"))
+        {
+            Cast();
+        }
+    }
+
+
+    void Cast()
+    {
+        if (istantanceFishingBob != null)
+        {
+            Destroy(istantanceFishingBob);
+        }
+        Vector3 cast_direction = new Vector3(cameraTransform.forward.x, -cameraTransform.forward.y, cameraTransform.forward.z);
+        fishing_state = FISHING_STATE.CASTING;
+        istantanceFishingBob = Instantiate(prefabFishingBob);
+        Physics.IgnoreCollision(GetComponent<BoxCollider>(), istantanceFishingBob.GetComponent<SphereCollider>());
+        istantanceFishingBob.transform.position = transform.position;
+        istantanceFishingBob.GetComponent<Rigidbody>().AddForce(cast_direction * fishingCastPowerMax,ForceMode.Impulse);
+    }
+    //void BeginCast()
+    //{
+
+    //}
+
+    //void EndCast()
+    //{
+
+    //}
+}
