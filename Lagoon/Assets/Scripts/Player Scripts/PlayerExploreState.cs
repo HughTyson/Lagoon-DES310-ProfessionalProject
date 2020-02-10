f5ea36c78c9ed8a1c623c86859357c8d27890f4d
Hugh <1700792@uad.ac.uk>
2020-02-10 02:31:41 +0000

    Conversation State and Supply Drop
    Conversation state created and teh supply drop has been started, couritine set up to make the box drop in a sequance

--- Lagoon/Assets/Scripts/Player Scripts/PlayerExploreState.cs
+++ Lagoon/Assets/Scripts/Player Scripts/PlayerExploreState.cs
@@ -8,25 +8,31 @@ public class PlayerExploreState : BaseState
     [SerializeField] CharacterControllerMovement movement_;
     [SerializeField] ThirdPersonCamera camera_;
 
-
     private void OnEnable()
     {
         movement_.current_state = CharacterControllerMovement.STATE.FREE_MOVEMENT;
         camera_.current_state = ThirdPersonCamera.STATE.FREE;
+        camera_.look_at_target = transform;
     }
 
     private void OnDisable()
     {
     }
 
-    private void Update()
+    // Update is called once per frame
+    public override void StateUpdate()
     {
-        if(Input.GetButtonDown("PlayerA"))
+        if (Input.GetButtonDown("PlayerA"))
         {
             StateManager.ChangeState(PlayerScriptManager.STATE.FISHING);
         }
+
+        if (Input.GetButtonDown("PlayerX"))
+        {
+            StateManager.ChangeState(PlayerScriptManager.STATE.CONVERSATION);
+        }
     }
 
-    
+
 
 }
