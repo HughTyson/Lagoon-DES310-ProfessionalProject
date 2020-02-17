999c9d8281335d68cdc47d927be0c8b9c4e55e33
H <h>
2020-02-17 14:20:14 +0000

    Player Repair State

---
+++ Lagoon/Assets/Player/States and Manager/PlayerRepairState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRepairState : BaseState
+{
+
+    enum PlaneSegments
+    {
+        PROPELLER,
+        ENGINE,
+        COCKPIT,
+        LEFTWING,
+        RIGHTWING,
+        FUSELAGE_LEFT_FRONT,
+        FUSELAGE_LEFT_MID,
+        FUSELAGE_LEFT_BACK,
+        FUSELAGE_RIGHT_FRONT,
+        FUSELAGE_RIGHT_MID,
+        FUSELAGE_RIGHT_BACK,
+        
+
+    }
+
+
+
+    public void OnEnable()
+    {
+    
+    }
+
+    public void OnDisable()
+    {
+        
+    }
+
+    public override void StateUpdate()
+    {
+        
+    }
+}
