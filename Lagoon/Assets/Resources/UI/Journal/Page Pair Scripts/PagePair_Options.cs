e04991ddf7e4a1a94128797a432e7d5cae91fc9d
Tomas Hudak <hudakt05@hotmail.com>
2020-04-09 20:02:45 +0100

    Starting work on journal options pages

--- Lagoon/Assets/Resources/UI/Journal/Page Pair Scripts/PagePair_Options.cs
+++ Lagoon/Assets/Resources/UI/Journal/Page Pair Scripts/PagePair_Options.cs
@@ -6,18 +6,54 @@ public class PagePair_Options : BasePagePair
 {
 
     [SerializeField] UnselectableButton goBackButton;
+    [SerializeField] SelectableButton_TextButton game_SButton;
+    [SerializeField] SelectableButton_TextButton control_SButton;
+    [SerializeField] SelectableButton_TextButton audio_SButton;
+    [SerializeField] SelectableButton_TextButton back_SButton;
 
 
+    [SerializeField] BasePagePair gameOptionPair;
+    [SerializeField] BasePagePair audioOptionPair;
+    [SerializeField] BasePagePair controlOptionPair;
 
     void Awake()
     {
         goBackButton.Event_Selected += request_GoBack;
+        back_SButton.Event_Selected += request_GoBack;
+
         goBackButton.SetButtonsToCheckForPress(new InputManager.BUTTON[] { InputManager.BUTTON.B });
+
+
+        game_SButton.Event_Selected += requestGoTo_GameOptions;
+        control_SButton.Event_Selected += requestGoTo_ControlOptions;
+        audio_SButton.Event_Selected += requestGoTo_AudioOptions;
+
+
     }
 
     public override void BegunEnteringPage()
     {
         goBackButton.Show();
+    }
+    public override void FinishedEnteringPage()
+    {
+        InfoRequest_CameraFromPage_Args args = Invoke_InfoRequest_CameFromPage();
+
+        //if (args.pageType == )
+    }
+
+
+    void requestGoTo_GameOptions()
+    {
+        Invoke_EventRequest_ChangePage(new RequestToChangePage(gameOptionPair));
+    }
+    void requestGoTo_AudioOptions()
+    {
+        Invoke_EventRequest_ChangePage(new RequestToChangePage(audioOptionPair));
+    }
+    void requestGoTo_ControlOptions()
+    {
+        Invoke_EventRequest_ChangePage(new RequestToChangePage(controlOptionPair));
     }
 
     void request_GoBack()
